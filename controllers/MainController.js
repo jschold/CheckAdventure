7b78e72b6f09942042a298a5c92360407acd30d0
jschold <jscholdny@msn.com>
2015-12-02 10:16:39 -0800

    fix partials

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,10 +1,23 @@
 checkAdventure.controller('MainCtrl', function MainCtrl($scope, MainFactory) {
-    // $scope.helloConsole = dataService.helloConsole;
+    $scope.helloConsole = dataService.helloConsole;
     $scope.list = MainFactory.list;
     $scope.MainFactory = MainFactory;
-    // $scope.findItems= dataService.findItems();
-    //
-    // dataService.findItems(function(response) {
-    //     $scope.todos = response.data;
-    // });
+    $scope.findItems= dataService.findItems();
+
+    dataService.findItems(function(response) {
+        $scope.todos = response.data;
+    });
 })
+
+checkAdventure.controller('AccordionDemoCtrl', function ($scope) {
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+});
