4e8784639fc1f2f2ed39d2602c4aab36870ee3a7
jschold <student@epicodus.com>
2015-12-10 16:39:18 -0800

    remove public folder

--- public/controllers/MainController.js
+++
@@ -1,9 +0,0 @@
-checkAdventure.controller("MainCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://checkadventure.firebaseio.com/");
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
-  // synchronize the object with a three-way data binding
-  syncObject.$bindTo($scope, "check");
-  // create an instance of the authentication service
-  // var auth = $firebaseAuth(ref);
-});
